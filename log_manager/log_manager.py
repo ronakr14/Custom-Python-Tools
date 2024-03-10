fbff653d19dd106c326e25bb53cc8bee41afc7e0
Ronak Rathore <ronak.rathore05@gmail.com>
2024-03-10 15:28:10 +0530

    b

--- log_manager/log_manager.py
+++ log_manager/log_manager.py
@@ -13,9 +13,11 @@ class LogManager:
         self.log_file = log_file
         self.log_level = log_level
         self.logger = logging.getLogger(__name__)
+        print("*****************")
+        print(self.logger)
         self.logger.setLevel(self.log_level)
 
-        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
 
         file_handler = logging.FileHandler(self.log_file)
         file_handler.setLevel(self.log_level)
