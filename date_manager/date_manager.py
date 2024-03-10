c746cb34cac77c59f552fd369a80948a73fe126b
Ronak Rathore <ronak.rathore05@gmail.com>
2024-03-10 17:47:27 +0530

    abc

--- date_manager/date_manager.py
+++ date_manager/date_manager.py
@@ -3,40 +3,53 @@ from log_manager.log_manager import LogManager
 
 
 class DateManager:
-    log = LogManager(log_name='DateManager')
+    def __init__(self):
+        self.log = LogManager(log_name='DateManager')
 
-    @staticmethod
-    def timestamp_to_date(dataframe: pd.DataFrame) -> pd.DataFrame:
+    def timestamp_to_date(self, dataframe: pd.DataFrame) -> pd.DataFrame:
         """
-            Convert datetime columns in a dataframe to date type.
+        Convert datetime columns in a dataframe to date type.
 
-            Parameters:
+        Args:
             dataframe (pd.DataFrame): The dataframe containing the datetime columns.
 
-            Returns:
+        Returns:
             pd.DataFrame: The input dataframe with the datetime columns converted to date type.
         """
-        datetime_columns = [col for col, dtype in dataframe.dtypes.items() if dtype == 'datetime64[ns]']
-        DateManager.log.info('Datetime columns successfully fetched from dataframe.')
-        DateManager.log.info(f"Datetime columns: {datetime_columns}")
+        datetime_columns = [
+            col for col, dtype in dataframe.dtypes.items() if dtype == 'datetime64[ns]'
+        ]
+
+        self.log.info('Datetime columns successfully fetched from dataframe.')
+        self.log.info(f"Datetime columns: {datetime_columns}")
+
         for col in datetime_columns:
             dataframe[col] = dataframe[col].dt.date
-            DateManager.log.info(f"Datetime column:{col} converted.")
+            self.log.info(f"Datetime column:{col} converted.")
 
         return dataframe
 
-    @staticmethod
-    def timestamp_to_date_column(column: str, dataframe: pd.DataFrame) -> pd.DataFrame:
+    def timestamp_to_date_column(self, column: str, dataframe: pd.DataFrame) -> pd.DataFrame:
         """
         Convert a datetime column in a dataframe to date type.
 
-        Parameters:
-        column (str): The name of the datetime column to convert.
-        dataframe (pd.DataFrame): The dataframe containing the datetime column.
+        Args:
+            column (str): The name of the datetime column to convert.
+            dataframe (pd.DataFrame): The dataframe containing the datetime column.
 
         Returns:
-        pd.DataFrame: The input dataframe with the datetime column converted to date type.
+            pd.DataFrame: The input dataframe with the datetime column converted to date type.
+
+        Raises:
+            ValueError: If the input dataframe does not contain the specified datetime column.
+
         """
-        dataframe[column] = dataframe[column].dt.date
-        DateManager.log.info(f"Datetime column:{column} converted.")
+        try:
+            if column not in dataframe.columns:
+                raise ValueError(
+                    f"The input dataframe does not contain the specified datetime column: {column}"
+                )
+            dataframe[column] = dataframe[column].dt.date
+        except ValueError as e:
+            self.log.error(e)
         return dataframe
